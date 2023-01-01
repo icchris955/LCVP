daf5291d2f3b45146b197f55856eeba48d365b05
izamha <glcode40@gmail.com>
2023-01-01 09:19:08 +0200

    Changed class to className

--- src/pages/index.js
+++ src/pages/index.js
@@ -34,9 +34,9 @@ export default function Index() {
         <div className="sticky top-0 h-screen flex flex-col items-center justify-center bg-black text-white">
           <h2 className="text-4xl">The Fourth Title</h2>
         </div>
-        <div class="w-full h-full fixed">
-          <div class="absolute bottom-0 right-0 mr-8 mb-32 ">
-            <div class="bg-white rounded-lg shadow-md px-4 py-2 border-1 border-gray-400">
+        <div className="w-full h-full fixed">
+          <div className="absolute bottom-0 right-0 mr-8 mb-32 ">
+            <div className="bg-white rounded-lg shadow-md px-4 py-2 border-1 border-gray-400">
               Was this doc helpful to you?
             </div>
           </div>
